17b2eb7917540d4b598ef36102211cb867e9d2d1
Chris Lee <chrislee187@gmail.com>
2020-12-03 01:22:39 +0000

    Extracted from Emma

--- src/Methodbrary/System/CharExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Methodbrary.System.String
-{
-    public static class CharExtensions
-    {
-        public static string Repeat(this char s, int times)
-            => new string(s, times);
-    }
-}
--- src/Methodbrary/System/TypeExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Methodbrary.System
-{
-    public static class TypeExtensions
-    {
-        public static bool IsStatic(this Type type) => type.IsAbstract && type.IsSealed;
-    }
-}
